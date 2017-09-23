43dad81e66c9a548df730bf1f58bdb8456531e58
BrannenGHall <Brannen@HurtadoHall.me>
2017-09-23 00:16:03 -0500

    Initial Commit

---
+++ TransitPlanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TransitPlanner
+//
+//  Created by Brannen Hall on 9/23/17.
+//  Copyright © 2017 BrannenGHH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
